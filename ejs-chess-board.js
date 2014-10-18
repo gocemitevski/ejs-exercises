59d48f4a873872ea398e6d7070f1d4394b7f935d
Goce Mitevski <goce.mitevski@gmail.com>
2014-10-18 21:10:58 +0200

    Update ejs-chess-board.js

--- ejs-chess-board.js
+++ ejs-chess-board.js
@@ -1,3 +1,7 @@
+// Write a program that creates a string that represents an 8x8 grid, using newline character to separate lines.
+// At each position of the grid, there is eirther a space or a "#" character.
+// The characters should form a chess board
+
 var size = 8;
 var mark = "";
 		
