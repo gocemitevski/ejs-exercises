eb02c3352089b76702bc32cda574a9b16f2feff7
Goce Mitevski <goce.mitevski@gmail.com>
2014-10-18 21:16:55 +0200

    Update ejs-fizz-buzz.js

--- ejs-fizz-buzz.js
+++ ejs-fizz-buzz.js
@@ -1,5 +1,6 @@
 // Write a program that uses console.log to print all numbers from 1 to 100, with two exceptions.
 // For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3) print "Buzz" instead.
+
 for (var number = 1; number <= 100; number++){
 
 		if (number % 3 == 0) {
